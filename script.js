5200db7c5981dfb3defa0de477da1f70604ded69
morciu <marianpralea@gmail.com>
2022-04-13 12:01:56 +0300

    Second attempt ant mimicing shelves with books. Added js function to add book to array.

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ let myLibrary = [];
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.read = read;
+    if (typeof pages !== 'number') {
+        this.pages = 0;
+    } else {
+        this.pages = pages;
+    }
+    if (typeof read !== 'boolean') {
+        this.read = false;
+    } else {
+        this.read = read;
+    }
 }
 
 
 // Add to library
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+// Open Add Menu
